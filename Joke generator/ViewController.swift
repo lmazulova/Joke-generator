6921377cc5d1cdc1ef9b1686d4b372c014bfd9c6
Elizabeth <lmazulova@bk.ru>
2024-10-01 13:39:30 +0300

    Initial Commit

---
+++ Joke generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joke generator
+//
+//  Created by user on 01.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
