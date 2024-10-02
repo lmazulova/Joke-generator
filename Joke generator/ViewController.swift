f7b6ee02c29eabd722527247573e603115d0ef60
Elizabeth <lmazulova@bk.ru>
2024-10-02 20:03:38 +0300

    Учебный проект, можно докрутить.

--- Joke generator/ViewController.swift
+++ Joke generator/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var typeView: UIView!
+    @IBOutlet weak var jokeIDView: UIView!
+    @IBOutlet weak var reloadButton: UIButton!
+    @IBOutlet weak var punchlineButton: UIButton!
+    
+    private func border(View: UIView){
+        View.layer.masksToBounds = true
+        View.layer.borderColor = UIColor.black.cgColor
+        View.layer.borderWidth = 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        border(View: cardView)
+        border(View: typeView)
+        border(View: jokeIDView)
+        border(View: reloadButton)
+        border(View: punchlineButton)
     }
-
-
+    
+    @IBAction func punchlineButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Punchline", message: "For fowl play", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
